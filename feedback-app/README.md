036bdcb5bdc48d156d89277fd0dfc9c4ff4f1767
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-13 08:27:52 +0100

    add svelte basics

--- feedback-app/README.md
+++ feedback-app/README.md
@@ -2,4 +2,9 @@
 
 A small project to understand Svelte core concepts.
 
+## Features
+
+- setting up a Svelte project.
+- understanding reactive values, conditional rendering and data mapping.
+
 Based on [Svelte Crash Course](https://www.youtube.com/watch?v=3TVy6GdtNuQ) by Brad Traversy (2021).
--- feedback-app/src/App.svelte
+++ feedback-app/src/App.svelte
@@ -1,30 +1,71 @@
 <script>
-	export let name;
+  // export let name;
+  let firstName = "Brad";
+  let lastName = "Doe";
+  let color = "blue";
+  let showText = false;
+  let users = [
+    {
+      id: "1",
+      name: "John",
+    },
+    {
+      id: "2",
+      name: "Sara",
+    },
+    {
+      id: "3",
+      name: "Bob",
+    },
+  ];
+
+  firstName = "John";
+
+  $: name = firstName + " " + lastName;
+
+  const toggle = () => {
+    color = color === "blue" ? "#ff3e00" : "blue";
+    showText = !showText;
+    users = [...users, { id: "4", name: "Jen" }];
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1 style="color: {color}">Hello {name}!</h1>
+
+  <!-- <button on:click={() => (color = "#ff3e00")}>Click</button> -->
+  <button on:click={toggle}>Click</button>
+  {#if showText}
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+      how to build Svelte apps.
+    </p>
+  {:else}
+    <p>No Text</p>
+  {/if}
+  {#each users as user (user.id)}
+    <h3>{user.id}: {user.name}</h3>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
