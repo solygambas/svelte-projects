eff74d12de31a40174e48d869e20d2bf18d66589
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-15 09:45:49 +0100

    add transitions

--- 01-feedback-app/README.md
+++ 01-feedback-app/README.md
@@ -10,5 +10,6 @@ A small project to understand Svelte core concepts.
 - forwarding events with createEventDispatcher.
 - counting reviews and displaying ratings average with reactive values.
 - handling user inputs with reactive values.
+- adding transitions with svelte/transition.
 
 Based on [Svelte Crash Course](https://www.youtube.com/watch?v=3TVy6GdtNuQ) by Brad Traversy (2021).
--- 01-feedback-app/src/components/FeedbackList.svelte
+++ 01-feedback-app/src/components/FeedbackList.svelte
@@ -1,8 +1,14 @@
 <script>
+  import { fade, scale } from "svelte/transition";
+
   import FeedbackItem from "./FeedbackItem.svelte";
+
   export let feedback = [];
 </script>
 
 {#each feedback as fb (fb.id)}
-  <FeedbackItem item={fb} on:delete-feedback />
+  <!-- <div transition:fade> -->
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} on:delete-feedback />
+  </div>
 {/each}
