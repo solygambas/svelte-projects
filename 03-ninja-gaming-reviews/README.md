16b9b3372c0d91558de17375e583ba7ac9f16b36
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-17 12:10:30 +0100

    add resetting layout

--- 03-ninja-gaming-reviews/README.md
+++ 03-ninja-gaming-reviews/README.md
@@ -10,6 +10,6 @@ A basic website using SvelteKit.
 
 - setting up a SvelteKit app.
 - understanding pages and routes.
-- creating a layout and reusing components.
+- creating a global layout, resetting it and reusing components.
 
 Based on [SvelteKit Crash Course Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP) by Shaun Pelling - The Net Ninja (2022).
---
+++ 03-ninja-gaming-reviews/src/routes/guides/__layout.reset.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Title from "$lib/title.svelte";
+  import "../../styles/global.css";
+</script>
+
+<header>
+  <nav>
+    <Title title="Ninja Gaming Guides" />
+    <div class="links">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/guides">Guides</a>
+    </div>
+  </nav>
+</header>
+<main>
+  <!-- page content -->
+  <slot />
+</main>
+<footer>
+  <p>Copyright {new Date().getFullYear()} Ninja Gaming Guides</p>
+</footer>
+
+<style>
+  header {
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  main {
+    max-width: 960px;
+    margin: 20px auto;
+  }
+  footer {
+    text-align: center;
+  }
+  .links {
+    margin-left: auto;
+  }
+  a {
+    margin-left: 10px;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  @media (max-width: 768px) {
+    nav {
+      flex-direction: column;
+    }
+    .links {
+      margin-left: 0;
+      margin-bottom: 5px;
+    }
+  }
+</style>
